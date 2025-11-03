5c361a7060a335b58630b7f207a08403ea01ccfe
charusiddharth16 <157917629+charusiddharth16@users.noreply.github.com>
2025-11-04 00:30:03 +0530

    Fix module.exports syntax in admin.js

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -5,4 +5,4 @@ router.post("/add-collage", adminController.addCollage);
 router.post("/add-category", adminController.addCategory);
 router.post("/delete-category", adminController.deleteCategory);
 
-module.exports = router;
+module.exports = router; 
